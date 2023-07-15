0d1d2a75534d4fb86d94085cae3ca49884516dff
nguyentieuat <thanhluan.kma@gmail.com>
2023-07-15 14:03:52 +0700

    update circle

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -6,6 +6,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
+  port: config.POSTGRES_PORT,
 
   dialect: "postgres",
   storage: ":memory:",
